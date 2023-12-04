6b06ccf107df2c0363106ba76f84ea237cb5607c
Mykola Marchenko <nickolay91509031@gmail.com>
2023-12-04 20:26:51 +0100

    feat: added cw4

---
+++ js/classworks/CW4/index.js
@@ -0,0 +1,25 @@
+// for (let i = 0; i >= 0; i *= 2) {
+//     console.log(i);
+// }
+
+// let sum = 0;
+// let i = 0;
+
+// while (i <= 9) {
+//     sum += i;
+//     console.log(sum);
+//     i++;
+// }
+
+let i = 1;
+
+while (i <= 15) {
+    console.log(i);
+    i += 2;
+
+    // if (i % 2 !== 0) {
+    //     console.log(i);
+    // }
+
+    // i++;
+}
