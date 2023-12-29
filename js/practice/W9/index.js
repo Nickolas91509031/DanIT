27a455df85b724ade0b5ae979824adf886014b3c
Mykola Marchenko <nickolay91509031@gmail.com>
2023-12-29 06:23:39 +0100

    feat: added w9

---
+++ js/practice/W9/index.js
@@ -0,0 +1,6 @@
+const listElement = document.getElementById("list");
+console.log(listElement);
+const listItem = document.getElementsByClassName("list-item");
+console.log(listItem);
+const listCssElementsAll = document.querySelector(".li");
+console.log(listCssElementsAll);
